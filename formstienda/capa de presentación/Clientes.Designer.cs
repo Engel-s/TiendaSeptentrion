b6033c3bcbd715e59eb60bd79546da4cf182ee8c
Ryuuga <elkingjaviergallegosgonzalez@gmail.com>
2025-04-16 17:29:47 -0600

    xd

--- formstienda/capa de presentación/Clientes.Designer.cs
+++ formstienda/capa de presentación/Clientes.Designer.cs
@@ -115,39 +115,21 @@
             // 
             // txttelefonocliente
             // 
-<<<<<<< HEAD
-            textBox1.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            textBox1.Location = new Point(118, 263);
-            textBox1.Margin = new Padding(2, 3, 2, 3);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(146, 27);
-            textBox1.TabIndex = 8;
-=======
             txttelefonocliente.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             txttelefonocliente.Location = new Point(118, 263);
             txttelefonocliente.Margin = new Padding(2, 3, 2, 3);
             txttelefonocliente.Name = "txttelefonocliente";
             txttelefonocliente.Size = new Size(146, 27);
             txttelefonocliente.TabIndex = 8;
->>>>>>> usuarios
             // 
             // txtnombrecliente
             // 
-<<<<<<< HEAD
-            textBox3.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            textBox3.Location = new Point(118, 178);
-            textBox3.Margin = new Padding(2, 3, 2, 3);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(214, 27);
-            textBox3.TabIndex = 10;
-=======
             txtnombrecliente.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             txtnombrecliente.Location = new Point(118, 178);
             txtnombrecliente.Margin = new Padding(2, 3, 2, 3);
             txtnombrecliente.Name = "txtnombrecliente";
             txtnombrecliente.Size = new Size(214, 27);
             txtnombrecliente.TabIndex = 10;
->>>>>>> usuarios
             // 
             // label6
             // 
@@ -162,59 +144,6 @@
             // 
             // txtdireccion
             // 
-<<<<<<< HEAD
-            richTextBox1.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            richTextBox1.Location = new Point(486, 378);
-            richTextBox1.Margin = new Padding(2, 3, 2, 3);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(310, 47);
-            richTextBox1.TabIndex = 12;
-            richTextBox1.Text = "";
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Nombre, Column2, Column3, Column4 });
-            dataGridView1.Location = new Point(14, 502);
-            dataGridView1.Margin = new Padding(2, 3, 2, 3);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.RowTemplate.Height = 28;
-            dataGridView1.Size = new Size(666, 353);
-            dataGridView1.TabIndex = 13;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "ID cliente";
-            Column1.MinimumWidth = 8;
-            Column1.Name = "Column1";
-            // 
-            // Nombre
-            // 
-            Nombre.HeaderText = "Nombre";
-            Nombre.MinimumWidth = 8;
-            Nombre.Name = "Nombre";
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "Teléfono ";
-            Column2.MinimumWidth = 8;
-            Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "Dirección";
-            Column3.MinimumWidth = 8;
-            Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            Column4.HeaderText = "Historial";
-            Column4.MinimumWidth = 8;
-            Column4.Name = "Column4";
-=======
             txtdireccion.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             txtdireccion.Location = new Point(486, 378);
             txtdireccion.Margin = new Padding(2, 3, 2, 3);
@@ -222,7 +151,6 @@
             txtdireccion.Size = new Size(310, 47);
             txtdireccion.TabIndex = 12;
             txtdireccion.Text = "";
->>>>>>> usuarios
             // 
             // button2
             // 
@@ -255,19 +183,6 @@
             // 
             // btnregistrar
             // 
-<<<<<<< HEAD
-            button3.BackColor = Color.FromArgb(3, 171, 229);
-            button3.Cursor = Cursors.Hand;
-            button3.FlatStyle = FlatStyle.Popup;
-            button3.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            button3.Location = new Point(46, 430);
-            button3.Margin = new Padding(2, 3, 2, 3);
-            button3.Name = "button3";
-            button3.Size = new Size(124, 35);
-            button3.TabIndex = 66;
-            button3.Text = "Registrar";
-            button3.UseVisualStyleBackColor = false;
-=======
             btnregistrar.BackColor = Color.FromArgb(3, 171, 229);
             btnregistrar.Cursor = Cursors.Hand;
             btnregistrar.FlatStyle = FlatStyle.Popup;
@@ -280,20 +195,9 @@
             btnregistrar.Text = "Registrar";
             btnregistrar.UseVisualStyleBackColor = false;
             btnregistrar.Click += btnregistrar_Click;
->>>>>>> usuarios
             // 
             // txtcedula
             // 
-<<<<<<< HEAD
-            richTextBox2.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            richTextBox2.Location = new Point(486, 306);
-            richTextBox2.Margin = new Padding(2, 3, 2, 3);
-            richTextBox2.Name = "richTextBox2";
-            richTextBox2.Size = new Size(284, 29);
-            richTextBox2.TabIndex = 68;
-            richTextBox2.Text = "";
-            richTextBox2.TextChanged += richTextBox2_TextChanged;
-=======
             txtcedula.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             txtcedula.Location = new Point(486, 323);
             txtcedula.Margin = new Padding(2, 3, 2, 3);
@@ -301,7 +205,6 @@
             txtcedula.Size = new Size(284, 29);
             txtcedula.TabIndex = 68;
             txtcedula.Text = "";
->>>>>>> usuarios
             // 
             // label8
             // 
@@ -318,11 +221,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-<<<<<<< HEAD
-            label1.Location = new Point(14, 366);
-=======
             label1.Location = new Point(14, 323);
->>>>>>> usuarios
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
             label1.Size = new Size(134, 20);
@@ -331,18 +230,6 @@
             // 
             // rbsi
             // 
-<<<<<<< HEAD
-            radioButton1.AutoSize = true;
-            radioButton1.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            radioButton1.Location = new Point(174, 368);
-            radioButton1.Margin = new Padding(2, 3, 2, 3);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(44, 24);
-            radioButton1.TabIndex = 72;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Si";
-            radioButton1.UseVisualStyleBackColor = true;
-=======
             rbsi.AutoSize = true;
             rbsi.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             rbsi.Location = new Point(174, 325);
@@ -353,22 +240,9 @@
             rbsi.Text = "Si";
             rbsi.UseVisualStyleBackColor = true;
             rbsi.CheckedChanged += radioButton1_CheckedChanged;
->>>>>>> usuarios
             // 
             // rbno
             // 
-<<<<<<< HEAD
-            radioButton2.AutoSize = true;
-            radioButton2.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            radioButton2.Location = new Point(230, 366);
-            radioButton2.Margin = new Padding(2, 3, 2, 3);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(53, 24);
-            radioButton2.TabIndex = 73;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "No";
-            radioButton2.UseVisualStyleBackColor = true;
-=======
             rbno.AutoSize = true;
             rbno.Checked = true;
             rbno.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
@@ -381,20 +255,9 @@
             rbno.Text = "No";
             rbno.UseVisualStyleBackColor = true;
             rbno.CheckedChanged += radioButton2_CheckedChanged;
->>>>>>> usuarios
             // 
             // lblcolilla
             // 
-<<<<<<< HEAD
-            label9.AutoSize = true;
-            label9.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            label9.Location = new Point(14, 310);
-            label9.Margin = new Padding(2, 0, 2, 0);
-            label9.Name = "label9";
-            label9.Size = new Size(109, 20);
-            label9.TabIndex = 74;
-            label9.Text = "Colilla INSS: ";
-=======
             lblcolilla.AutoSize = true;
             lblcolilla.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             lblcolilla.Location = new Point(14, 378);
@@ -403,25 +266,15 @@
             lblcolilla.Size = new Size(109, 20);
             lblcolilla.TabIndex = 74;
             lblcolilla.Text = "Colilla INSS: ";
->>>>>>> usuarios
             // 
             // txtcolillainss
             // 
-<<<<<<< HEAD
-            textBox2.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            textBox2.Location = new Point(140, 310);
-            textBox2.Margin = new Padding(2, 3, 2, 3);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(214, 27);
-            textBox2.TabIndex = 75;
-=======
             txtcolillainss.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             txtcolillainss.Location = new Point(140, 378);
             txtcolillainss.Margin = new Padding(2, 3, 2, 3);
             txtcolillainss.Name = "txtcolillainss";
             txtcolillainss.Size = new Size(214, 27);
             txtcolillainss.TabIndex = 75;
->>>>>>> usuarios
             // 
             // label5
             // 
@@ -436,21 +289,12 @@
             // 
             // txtapellidocliente
             // 
-<<<<<<< HEAD
-            textBox5.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
-            textBox5.Location = new Point(118, 223);
-            textBox5.Margin = new Padding(2, 3, 2, 3);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(214, 27);
-            textBox5.TabIndex = 77;
-=======
             txtapellidocliente.Font = new Font("Calisto MT", 10F, FontStyle.Bold);
             txtapellidocliente.Location = new Point(118, 223);
             txtapellidocliente.Margin = new Padding(2, 3, 2, 3);
             txtapellidocliente.Name = "txtapellidocliente";
             txtapellidocliente.Size = new Size(214, 27);
             txtapellidocliente.TabIndex = 77;
->>>>>>> usuarios
             // 
             // dateTimePicker1
             // 
--- formstienda/capa de presentación/Clientes.cs
+++ formstienda/capa de presentación/Clientes.cs
@@ -1,16 +1,6 @@
 ﻿using formstienda.capa_de_negocios;
 using formstienda.Datos;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using formstienda.capa_de_negocios;
-using formstienda.Datos;
 
 namespace formstienda.capa_de_presentación
 {
